54a80426534ea4942426819259a75ab548980543
pink-eye <62290775+pink-eye@users.noreply.github.com>
2020-08-23 13:05:46 +0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,22 +5,13 @@ $(document).ready(function(){
 	let burger = document.getElementById("burger");
 	let menu = document.getElementById("menu");
 
-	if(innerWidth < 768)
-	{
-		burger.onclick = function() {
-			burger.classList.toggle("header__burger_active");
-			menu.classList.toggle("header-menu_active");
-			if(burger.classList.contains('header__burger_active'))
-				document.body.style.overflow = 'hidden';
-			else
-				document.body.style.overflow = 'visible';
-		}
-	}
-	else
-	{
-		burger.classList.remove("header__burger_active");
-		menu.classList.remove("header-menu_active");
-		document.body.style.overflow = 'visible';
+	burger.onclick = function() {
+		burger.classList.toggle("header__burger_active");
+		menu.classList.toggle("header-menu_active");
+		if(burger.classList.contains('header__burger_active'))
+			document.body.style.overflow = 'hidden';
+		else
+			document.body.style.overflow = 'visible';
 	}
 
 	// SETTINGS OF SLICK SLIDER
